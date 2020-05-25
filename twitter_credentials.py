1dd6fa24af6851c32f09315ff9bdf11257d73374
dhilanr <38360278+dhilanr@users.noreply.github.com>
2020-05-24 20:50:12 -0400

    sentiment analysis added

---
+++ twitter_credentials.py
@@ -0,0 +1,10 @@
+######  AUTHENTICATION CREDENTIALS  ###### 
+
+
+ACCESS_TOKEN = "insert here"
+ACCESS_TOKEN_SECRET = "insert here"
+CONSUMER_KEY = "insert here"
+CONSUMER_SECRET = "insert here"
+
+## only used for auto-reply DMs
+PERSONAL_ID = insert-value-here
